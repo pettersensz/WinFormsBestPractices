edca30a533e80358c4843651ac0c29618848dddd
Sigmund Pettersen <sigmund.pettersen@cegal.com>
2020-09-07 13:47:09 +0200

    More interfaces

--- PluralsightWinFormsDemoApp/Views/EpisodeView.cs
+++ PluralsightWinFormsDemoApp/Views/EpisodeView.cs
@@ -1,9 +1,10 @@
-﻿using System.Windows.Forms;
+﻿using System.Runtime.Remoting.Messaging;
+using System.Windows.Forms;
 using PluralsightWinFormsDemoApp.Resources;
 
 namespace PluralsightWinFormsDemoApp.Views
 {
-    public partial class EpisodeView : UserControl
+    public partial class EpisodeView : UserControl, IEpisodeView
     {
         public EpisodeView()
         {
@@ -16,5 +17,51 @@ namespace PluralsightWinFormsDemoApp.Views
         {
             MessageBox.Show(TextResources.TagsHelp);
         }
+
+        public string Description
+        {
+            get => labelEpisodeDescription.Text; 
+            set => labelEpisodeDescription.Text = value;
+        }
+
+        public string Title
+        {
+            get => labelEpisodeTitle.Text; 
+            set => labelEpisodeTitle.Text = value;
+        }
+
+        public string PublicationDate
+        {
+            get => labelPublicationDate.Text; 
+            set => labelPublicationDate.Text = value;
+        }
+
+        public int Rating
+        {
+            get => (int) numericUpDownRating.Value;
+            set => numericUpDownRating.Value = value;
+        }
+        
+        public string Notes
+        {
+            get => textBoxNotes.Text;
+            set => textBoxNotes.Text = value;
+        }
+        
+        public string Tags
+        {
+            get => textBoxTags.Text;
+            set => textBoxTags.Text = value;
+        }
+    }
+
+    public interface IEpisodeView
+    {
+        string Description { set; }
+        string Title { set; }
+        string PublicationDate { set; }
+        int Rating { get; set; }
+        string Notes { get; set; }
+        string Tags { get; set; }
     }
 }
--- PluralsightWinFormsDemoApp/Views/SubscriptionView.cs
+++ PluralsightWinFormsDemoApp/Views/SubscriptionView.cs
@@ -1,20 +1,49 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Drawing;
-using System.Data;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace PluralsightWinFormsDemoApp
 {
-    public partial class SubscriptionView : UserControl
+    public partial class SubscriptionView : UserControl, ISubscriptionView
     {
         public SubscriptionView()
         {
             InitializeComponent();
+            treeViewPodcasts.AfterSelect += (s, a) => OnSelectionChanged();
         }
+
+        public TreeNode SelectedNode => treeViewPodcasts.SelectedNode;
+
+        public void AddNode(TreeNode treeNode)
+        {
+            treeViewPodcasts.Nodes.Add(treeNode);
+        }
+
+        public void RemoveNode(string key)
+        {
+            var node = treeViewPodcasts.Nodes[key];
+            treeViewPodcasts.Nodes.Remove(node);
+        }
+
+        public void SelectNode(string key)
+        {
+            treeViewPodcasts.SelectedNode = treeViewPodcasts.Nodes[key];
+        }
+
+        public event EventHandler SelectionChanged;
+
+        protected virtual void OnSelectionChanged()
+        {
+            var handler = SelectionChanged;
+            handler?.Invoke(this,EventArgs.Empty);
+        }
+    }
+
+    public interface ISubscriptionView
+    {
+        TreeNode SelectedNode { get; }
+        void AddNode(TreeNode treeNode);
+        void RemoveNode(string key);
+        void SelectNode(string key);
+        event EventHandler SelectionChanged;
     }
 }
