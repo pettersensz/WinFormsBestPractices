c0bddf11d6d25a96313cfd984a8be1ccfe6b658a
Sigmund Pettersen <sigmund.pettersen@cegal.com>
2020-09-08 13:03:53 +0200

    Implement horizontal scrollbar

--- PluralsightWinFormsDemoApp/Views/WaveFormViewer.cs
+++ PluralsightWinFormsDemoApp/Views/WaveFormViewer.cs
@@ -1,11 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
 using System.Drawing;
-using System.Data;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace PluralsightWinFormsDemoApp.Views
@@ -15,17 +9,41 @@ namespace PluralsightWinFormsDemoApp.Views
         private float[] _peaks;
         private Brush _backBrush;
         private Pen _waveformPen;
-        
+
         public WaveFormViewer()
         {
             InitializeComponent();
             DoubleBuffered = true;
+            hScrollBar1.Scroll += hScrollBar1_Scroll;
         }
+
+        private void hScrollBar1_Scroll(object sender, ScrollEventArgs e)
+        {
+            Invalidate();
+        }
+
 
         public void SetPeaks(float[] newPeaks)
         {
             _peaks = newPeaks;
+            CalculateScrollbar();
             Invalidate();
+        }
+
+        private void CalculateScrollbar()
+        {
+            if (_peaks != null)
+            {
+                hScrollBar1.Maximum = _peaks.Length;
+                hScrollBar1.LargeChange = Width;
+                hScrollBar1.SmallChange = Width / 10;
+            }
+        }
+
+        protected override void OnResize(EventArgs e)
+        {
+            base.OnResize(e);
+            CalculateScrollbar();
         }
 
         protected override void OnPaint(PaintEventArgs e)
@@ -35,12 +53,16 @@ namespace PluralsightWinFormsDemoApp.Views
             {
                 _backBrush = _backBrush ?? new SolidBrush(BackColor);
                 _waveformPen = _waveformPen ?? new Pen(ForeColor);
+
+                var startPeak = hScrollBar1.Value;
+
                 e.Graphics.FillRectangle(_backBrush, ClientRectangle);
-                for (int x = 0; x < _peaks.Length && x < Width; x++)
+                for (int x = 0; (startPeak + x < _peaks.Length) && x < Width; x++)
                 {
-                    var height = _peaks[x] * Height;
-                    var top = (Height - height) / 2;
-                    e.Graphics.DrawLine(_waveformPen, x, top,x,top+height);
+                    var availableHeight = Height - hScrollBar1.Height;
+                    var height = _peaks[startPeak + x] * availableHeight;
+                    var top = (availableHeight - height) / 2;
+                    e.Graphics.DrawLine(_waveformPen, x, top, x, top + height);
                 }
             }
         }
